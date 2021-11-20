e844b4e20f1833a19335ddda11e00bd4db76f661
Molnár Botond <45335158+Gepsonka@users.noreply.github.com>
2021-11-20 02:00:16 +0100

    xd

--- prototyping.py
+++ prototyping.py
@@ -4,25 +4,13 @@ import mediapipe as mp
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 
-# Load the gesture recognizer model
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(max_num_hands=1, min_detection_confidence=0.7)
+mpDraw = mp.solutions.drawing_utils
+
 model = load_model('mp_hand_gesture')
 # Load class names
 f = open('gesture.names', 'r')
 classNames = f.read().split('\n')
 f.close()
 print(classNames)
-
-# Initialize the webcam for Hand Gesture Recognition Python project
-cap = cv2.VideoCapture(0)
-while True:
-    _, frame = cap.read()
-    x, y, c = frame.shape
-  # Flip the frame vertically
-    frame = cv2.flip(frame, 1)
-  # Show the final output
-    cv2.imshow("Output", frame)
-    if cv2.waitKey(1) == ord('q'):
-        break
-# release the webcam and destroy all active windows
-cap.release()
-cv2.destroyAllWindows()
